4588ea8cf1f31354dc4b48ec5bcf301421e42a02
huangtiecheng <896574716@qq.com>
2021-01-12 22:34:23 +0800

    提交v1.2.1修改

--- src/main/java/com.htc.demo/Demo.java
+++ src/main/java/com.htc.demo/Demo.java
@@ -6,6 +6,7 @@ public class Demo {
         System.out.println("this V1.1");
         System.out.println("this master");
         System.out.println("this V1.2");
+        System.out.println("this V1.2.1");
     }
 
 }
