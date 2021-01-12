0d97ee878edcb876e97c8dab2f575e752bdbe6af
huangtiecheng <896574716@qq.com>
2021-01-12 22:11:01 +0800

    提交v1.1修改

--- src/main/java/com.htc.demo/Demo.java
+++ src/main/java/com.htc.demo/Demo.java
@@ -3,8 +3,7 @@ package com.htc.demo;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("main");
-        System.out.println("main1");
+        System.out.println("this V1.1");
     }
 
 }
